bb086d5d9939bdd224e4b6c8c8238cfc32c4b209
Lucian Condrea <luciancondreadev@gmail.com>
2019-07-30 20:11:44 -0400

    Add testing framework

---
+++ App.test.js
@@ -0,0 +1,9 @@
+import { render } from 'react-native-testing-library';
+import React from 'react';
+import { View } from 'react-native';
+
+describe('app', () => {
+    test('renders correctly a view', () => {
+        render(<View />);
+    })
+})
